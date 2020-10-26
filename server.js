6b6d1522e668ab70a769b46fd355b56f7864d1ee
Ed Ignot <edignot@gmail.com>
2020-10-26 16:09:04 -0600

    Connected to the database

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+const mongoose = require('mongoose')
 const app = require('./app')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully')
+})
 
 const port = process.env.PORT || 2000
 
