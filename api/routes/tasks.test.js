8839f2645f8dfeeaa438774d4420122ac470eca4
Ed Ignot <edignot@gmail.com>
2020-10-26 23:02:53 -0600

    COnnected to local MongoDB database

--- api/routes/tasks.test.js
+++ api/routes/tasks.test.js
@@ -1,6 +1,14 @@
 const app = require('../../app')
 const supertest = require('supertest')
 const request = supertest(app)
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+
+beforeAll(async () => {
+  const uri = `mongodb://127.0.0.1/tasks`
+  mongoose.connect(uri, { useNewUrlParser: true })
+})
 
 it('Gets the test endpoint', async (done) => {
   const res = await request.get('/something')
