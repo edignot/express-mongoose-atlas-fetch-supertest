2119698d830112855036d7986e94bdd16daad717
Ed Ignot <edignot@gmail.com>
2020-10-26 20:44:48 -0600

    Not found route

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.use(express.json())
 
 app.use('/api/tasks', require('./api/routes/tasksRouter'))
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not found' })
+})
+
 module.exports = app
