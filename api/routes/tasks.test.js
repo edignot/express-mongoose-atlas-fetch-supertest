3e4aa8e61dd0c24dca36233875a969090a936f36
Ed Ignot <edignot@gmail.com>
2020-10-27 12:39:42 -0600

    After all hook added

--- api/routes/tasks.test.js
+++ api/routes/tasks.test.js
@@ -4,30 +4,45 @@ const request = supertest(app)
 const mongoose = require('mongoose')
 const Task = require('../models/tasksModel')
 
-beforeAll(async () => {
-  const url = `mongodb://127.0.0.1/tasks`
-  await mongoose.connect(url, { useNewUrlParser: true })
-})
-
-afterEach(async () => {
-  await Task.deleteMany()
-})
-
-it('Gets the test endpoint', async (done) => {
-  const res = await request.get('/')
-  expect(res.status).toBe(404)
-  expect(res.body.error).toBe('Not found')
-  done()
-})
-
-it('Should save a task to database', async (done) => {
-  const res = await request.post('/api/tasks').send({
-    title: 'TESTINGSS',
-    task: 'another',
+describe('Task', () => {
+  beforeAll(async () => {
+    const url = `mongodb://127.0.0.1/tasks`
+    await mongoose.connect(url, { useNewUrlParser: true })
   })
-  expect(res.status).toBe(200)
-  const task = await Task.findOne({ title: 'TESTINGSS' })
 
-  console.log(task)
-  done()
+  afterEach(async () => {
+    await Task.deleteMany()
+  })
+
+  afterAll(async () => {
+    await Task.drop()
+    await mongoose.connection.close()
+  })
+
+  test('Gets the test endpoint', async (done) => {
+    const res = await request.get('/')
+    expect(res.status).toBe(404)
+    expect(res.body.error).toBe('Not found')
+    done()
+  })
+
+  test('Should save a task to database', async (done) => {
+    const res = await request.post('/api/tasks').send({
+      title: 'test title',
+      task: 'test task',
+    })
+
+    expect(res.status).toBe(200)
+
+    const task = await Task.findOne({ title: 'test title' })
+    expect(task.title).toBeTruthy()
+    expect(task.title).toBe('test title')
+    expect(task.task).toBeTruthy()
+    expect(task.task).toBe('test task')
+    expect(task.image).toBeTruthy()
+
+    console.log(task)
+
+    done()
+  })
 })
