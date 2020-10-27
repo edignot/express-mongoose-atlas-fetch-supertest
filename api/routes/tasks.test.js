231b788af9781a94f09c5eb867bb7f29b564237c
Ed Ignot <edignot@gmail.com>
2020-10-27 11:53:40 -0600

    Before and after each testing functions reseting database

--- api/routes/tasks.test.js
+++ api/routes/tasks.test.js
@@ -1,29 +1,32 @@
 const app = require('../../app')
 const supertest = require('supertest')
 const request = supertest(app)
-const dotenv = require('dotenv')
-dotenv.config()
 const mongoose = require('mongoose')
 const Task = require('../models/tasksModel')
 
 beforeAll(async () => {
-  const uri = `mongodb://127.0.0.1/tasks`
-  mongoose.connect(uri, { useNewUrlParser: true })
+  const url = `mongodb://127.0.0.1/tasks`
+  await mongoose.connect(url, { useNewUrlParser: true })
+})
+
+afterEach(async () => {
+  await Task.deleteMany()
 })
 
 it('Gets the test endpoint', async (done) => {
   const res = await request.get('/')
+  expect(res.status).toBe(404)
   expect(res.body.error).toBe('Not found')
   done()
 })
 
 it('Should save a task to database', async (done) => {
   const res = await request.post('/api/tasks').send({
-    title: 'TESTINGS',
+    title: 'TESTINGSS',
     task: 'another',
   })
-
-  const task = await Task.findOne({ title: 'TESTINGS' })
+  expect(res.status).toBe(200)
+  const task = await Task.findOne({ title: 'TESTINGSS' })
 
   console.log(task)
   done()
