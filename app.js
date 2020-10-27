7eebed4ba13ee58c91a56f3da6e75dea5ff59e29
Ed Ignot <edignot@gmail.com>
2020-10-27 13:09:53 -0600

    Database error hanler middleware deleted

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ app.use('/api/tasks', require('./api/routes/tasksRouter'))
 
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Not found' })
+  next()
 })
-
-// app.use((error, req, res, next) => {
-//   res.status(500).json({ error: error.message })
-// })
 
 module.exports = app
