a6f7ee7f8697df3bdd7dd79863671b5e4d030e9f
Ed Ignot <edignot@gmail.com>
2020-10-27 13:30:36 -0600

    Get all tasks from database test passing

--- api/tests/tasks.test.js
+++ api/tests/tasks.test.js
@@ -14,6 +14,9 @@ describe('Task', () => {
   beforeAll(async () => {
     const url = `mongodb://127.0.0.1/tasks`
     await mongoose.connect(url, { useNewUrlParser: true })
+  })
+
+  beforeEach(async () => {
     for (const task of tasks) {
       const newTask = new Task(task)
       await newTask.save()
@@ -29,30 +32,34 @@ describe('Task', () => {
     await mongoose.connection.close()
   })
 
-  test('Gets the test endpoint', async (done) => {
-    const res = await request.get('/')
-    expect(res.status).toBe(404)
-    expect(res.body.error).toBe('Not found')
-    done()
-  })
-
   test('Should save a task to database', async (done) => {
     const res = await request.post('/api/tasks').send({
       title: 'test title',
       task: 'test task',
     })
-
     expect(res.status).toBe(200)
-
     const task = await Task.findOne({ title: 'test title' })
-    expect(task.title).toBeTruthy()
     expect(task.title).toBe('test title')
-    expect(task.task).toBeTruthy()
     expect(task.task).toBe('test task')
     expect(task.image).toBeTruthy()
+    expect(task._id).toBeTruthy()
+    done()
+  })
 
-    console.log(task)
+  test('Should get all tasks from a database', async (done) => {
+    const res = await request.get('/api/tasks')
+    expect(res.status).toBe(200)
+    const parsedRes = JSON.parse(res.text)
+    expect(parsedRes.length).toBe(tasks.length)
+    expect(parsedRes[0].title).toBe(tasks[0].title)
+    expect(parsedRes[0].task).toBe(tasks[0].task)
+    done()
+  })
 
+  test('Gets the test endpoint', async (done) => {
+    const res = await request.get('/')
+    expect(res.status).toBe(404)
+    expect(res.body.error).toBe('Not found')
     done()
   })
 })
