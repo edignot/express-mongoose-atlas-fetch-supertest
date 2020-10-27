ced41974b88cb19076b2e314e3cab6960fed44da
Ed Ignot <edignot@gmail.com>
2020-10-26 23:31:45 -0600

    Posting to testing database

--- api/routes/tasks.test.js
+++ api/routes/tasks.test.js
@@ -4,6 +4,7 @@ const request = supertest(app)
 const dotenv = require('dotenv')
 dotenv.config()
 const mongoose = require('mongoose')
+const Task = require('../models/tasksModel')
 
 beforeAll(async () => {
   const uri = `mongodb://127.0.0.1/tasks`
@@ -11,7 +12,19 @@ beforeAll(async () => {
 })
 
 it('Gets the test endpoint', async (done) => {
-  const res = await request.get('/something')
+  const res = await request.get('/')
   expect(res.body.error).toBe('Not found')
+  done()
+})
+
+it('Should save a task to database', async (done) => {
+  const res = await request.post('/api/tasks').send({
+    title: 'TESTINGS',
+    task: 'another',
+  })
+
+  const task = await Task.findOne({ title: 'TESTINGS' })
+
+  console.log(task)
   done()
 })
