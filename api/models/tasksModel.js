874b6b83ca3894623225714dffb5efec10f0b6df
Ed Ignot <edignot@gmail.com>
2020-10-26 18:55:16 -0600

    Task model

---
+++ api/models/tasksModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const tasksSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 5,
+    },
+    task: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+    },
+  },
+  { timestamps: true },
+)
+
+const Task = mongoose.model('Task', tasksSchema)
+
+module.exports = Task
